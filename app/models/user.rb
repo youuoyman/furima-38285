5fdfdd036ef538e583325a1940c9ffce4e4379d3
youuoyman <mimisuke0926@gmail.com>
2022-11-12 14:47:04 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  #新規登録/ユーザー情報
   validates :nickname, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  #新規登録/本人情報確認
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true

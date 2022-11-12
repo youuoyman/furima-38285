134e9dd08eb7debc410af4cee4fe6d999b98d91a
youuoyman <mimisuke0926@gmail.com>
2022-11-12 18:05:51 +0900

    バリデーション修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,4 @@
 class TopController < ApplicationController
   def index
-    @users = User.all
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # 新規登録/ユーザー情報
-  validates :nickname, presence: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
-  # 新規登録/本人情報確認
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
-end
+          
+         
+  with_options presence: true do
+    validates :nickname, :birthday, :password_confirmation
+    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }
+    validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい" }
+  end
+    validates :password, :password_confirmation,
+    format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "は半角英数で入力して下さい" }
+  end
+end       

9e85d6f1ef9e45f0f4153b3ad5a22bab9f38b0b4
youuoyman <mimisuke0926@gmail.com>
2022-11-12 18:19:42 +0900

    半角英数字を1文字含める

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { Faker::Internet.password(min_alpha: 1, min_numric: 1, min_length: 6) }
     password_confirmation { password }
     last_name             { '山田' }
     first_name            { '太郎' }

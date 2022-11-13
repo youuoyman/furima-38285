895f043e4cfacdd8a15200767fb9170a6a76bedf
youuoyman <mimisuke0926@gmail.com>
2022-11-13 14:12:05 +0900

    正規表現修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          
    with_options presence: true do
     validates :nickname, :birthday, :password_confirmation
-    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }
+    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }
     validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい" }
   end
     validates :password, :password_confirmation,

a575bc95b2c63cb68efc7d94a515f69664f6b03f
youuoyman <mimisuke0926@gmail.com>
2022-11-13 14:25:39 +0900

    メールアドレスが一意性である

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -39,6 +39,12 @@ RSpec.describe User, type: :model do
        @user.valid?
        expect(@user.errors.full_messages).to include 'Email is invalid'
      end
+     it '重複したemailが存在する場合登録出来ない' do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include "Email has already been taken"
+     end
      it 'passwordが5文字以下では登録できない' do
        @user.password = '00000'
        @user.password_confirmation = '00000'

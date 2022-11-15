7f998a607689079f2882d2c6c77b61e588f88cd4
youuoyman <mimisuke0926@gmail.com>
2022-11-15 12:12:26 +0900

    出品画像投稿の生成と保存設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
   def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
   end
 end

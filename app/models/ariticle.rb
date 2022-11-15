bb78b94f4a576031b63a90a71fa6f8eaeb1fc3e9
youuoyman <mimisuke0926@gmail.com>
2022-11-15 13:02:15 +0900

    articleのバリデーション設定

--- app/models/ariticle.rb
+++ app/models/ariticle.rb
@@ -1,4 +1,11 @@
 class Ariticle < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
+
+ #空の投稿保存できない
+ validates :title, text, presence: true
+
+ #---の時保存できない
+ validates :genre_id, numericality: { other_than: 1 }
+
 end

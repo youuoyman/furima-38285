b7b52eaef6af0a3f3cb393b9b55990a8a1c3c60c
youuoyman <mimisuke0926@gmail.com>
2022-11-15 22:10:36 +0900

    MF編集とアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
 end
--- db/migrate/20221113062151_create_items.rb
+++ db/migrate/20221113062151_create_items.rb
@@ -10,6 +10,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer         :delivery_id,          null: false
       t.integer         :item_price,           null: false
       t.references      :user,                 null: false, foreign_key: true
+      t.integer         :genre_id,             null: false
 
       t.timestamps
     end

9eeb74caeed13f416357e7c2ac6802d1ad8aa860
youuoyman <mimisuke0926@gmail.com>
2022-11-17 10:08:15 +0900

    ジャンル選択設定（完了→トップ、否→new）

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,10 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
+
+  #空の投稿を保存できない
+  validates :image, :item_name, :item_info, :item_category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_id, :item_price, presence: true
+  #ジャンルの選択---保存できない
+  validates :genre_id, numericality: { other_than: 1, message: "can't be blank"}
+
 end

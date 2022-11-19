05cee826de6d3c2615cca8cc3275b618cf57b763
youuoyman <mimisuke0926@gmail.com>
2022-11-19 11:14:44 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,16 +10,12 @@ class Item < ApplicationRecord
   belongs_to :delivery
 
   #空の投稿を保存できない
-  with_options: presence: true
-    validates :image, :item_name, :item_info, :item_category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_id, :item_price
-  end
+  validates :image, :item_name, :item_info, :item_category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_id, :item_price, presence: true
+  #---は保存できない
+  validates :item_category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_id, numericality: { other_than: 1, message: "can't be blank"}
   #金額のバリデーション
-  with_options: presence: true, format: { with: /\A[0-9]+\z/ } do
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
     validates :item_price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                           presecse: { message: "can't be blank" }
-  end
-  #選択が---の時保存できない
-  with_options: presecse: true
-    validates :item_category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_id, numericality: { other_than: 1, message: "can't be blank"}
+                           presence: { message: "can't be blank" }
   end
 end

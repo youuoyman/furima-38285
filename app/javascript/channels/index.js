e4042d912d643f030ee6429cde09fc449d3f3b83
youuoyman <mimisuke0926@gmail.com>
2022-11-19 14:53:30 +0900

    手数料と利益　表示変更

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,14 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+window.addEventListener('load', function(){
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", function(){
+  const addTax = document.getElementById("add-tax-price");
+  addTax.innerHTML = Math.round(priceInput.value * 0.1);
+  const addProfit = document.getElementById("profit");
+  console.log(priceInput.innerHTML);
+  addProfit.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+ })
+});

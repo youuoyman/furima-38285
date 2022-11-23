a015a97f64c646b796ac8b8423dd690a4666c1fb
youuoyman <mimisuke0926@gmail.com>
2022-11-23 15:13:43 +0900

    商品削除機能設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :correct_item, only: [:edit]
  
 
@@ -31,6 +31,15 @@ class ItemsController < ApplicationController
   def update
     if @item.update(item_params)
       redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
     else
       render :edit
     end

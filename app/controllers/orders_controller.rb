20dbd20ba223b64a03b22e4431646340269b64f7
youuoyman <mimisuke0926@gmail.com>
2022-11-27 12:24:28 +0900

    コントローラー設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,24 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @order = Order.new
   end
 
+
   def create
+    @item = Item.find(params[:item_id])
+    @order = OrderForm.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render :index
+    end
   end
-  
+
+  private
+  def order_params
+    params.require(:order).permit(:portal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
 end

cd578080f441b4267f54dbbaae3b6bcf7e07845d
youuoyman <mimisuke0926@gmail.com>
2022-11-29 18:49:09 +0900

    フォームオブジェクト設定

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,19 +1,24 @@
 class OrderForm
   include ActiveModel::Model #トークンが必要になるかも
-  attr_accessor :portal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
-    
-  #バリデーション
-  validates :prefecture_id, :city, :addresses, presence: true
+  attr_accessor :portal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :portal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "良い例:123-4567 良くない例:1234567"}
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "良い例:09012345678 良くない例:090-1234-5678"}
-    validates :prefecture_id, numericality: { order_than: 0, message: "選択してください"}
+    #orderモデル
+    validates :user_id
+    validates :item_id
+    #paymentモデル
+    validates :portal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }, length: { is: 8 }
+    validates :prefecture_id, numericality: { order_than: 0 }
+    validates :city
+    validates :addresses
+    validates :phone_number, format: { with: /\A\d{10,11}\z/}, length: { maximum: 11}
+    #トークン
+    validates :token
   end
 
-  def save
-    @orders = Order.create(user_id: user_id, item_id: item_id)
-    Payment.create(portal_code: portal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
 
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Payment.create(portal_code: portal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
   end
 end

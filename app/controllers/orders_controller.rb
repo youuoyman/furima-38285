0f3e53ba95e96ba3f0e7955d4a4301dbb183d6d8
youuoyman <mimisuke0926@gmail.com>
2022-12-01 19:14:08 +0900

    削除（before_actionで定義されている）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,7 +25,6 @@ class OrdersController < ApplicationController
   end
 
   def non_rog
-    @item = Item.find(params[:item_id])
     redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 

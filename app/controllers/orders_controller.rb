9dd1c41433062be70a3cff9180b758016b1b0573
youuoyman <mimisuke0926@gmail.com>
2022-12-01 12:54:53 +0900

    コントローラー再設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,18 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :non_rog, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
-    @order = Order.new
+      @order_form = OrderForm.new
   end
 
 
   def create
-    @item = Item.find(params[:item_id])
-    @order = OrderForm.new(order_params)
-    if @order.valid?
-      @order.save
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
       return redirect_to root_path
     else
       render :index
@@ -18,7 +21,24 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:portal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_form).permit(:portal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
+  def non_rog
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.item_price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
+  end
 end

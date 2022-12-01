17789ffb296233210d8e959b1ea58defa6fcdb79
youuoyman <mimisuke0926@gmail.com>
2022-12-01 15:34:41 +0900

    詳細ページ　ログイン設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    if @item.user_id == current_user.id && @item.order.nil?
+    else
+      redirect_to root_path
+    end
   end
 
   def update
